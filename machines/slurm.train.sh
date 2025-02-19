52919a6fb171082810a3168a9f27568268732df3
Leif Denby <lcd@dmi.dk>
2025-02-19 16:41:57 +0100

    more cleanup and tweaks

--- machines/slurm.train.sh
+++ machines/slurm.train.sh
@@ -5,15 +5,14 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --gres=gpu:8  #per node
 #SBATCH --no-requeue
-#SBATCH --partition=prodq
+#SBATCH --partition=defq
 #SBATCH --exclusive
 #SBATCH --account=cu_0003
-#SBATCH --output=/dcai/users/schhau/git-repos/mllam-exps/logs/neurallam.%j.log
-#SBATCH --error=/dcai/users/schhau/git-repos/mllam-exps/logs/neurallam.%j.log
+#SBATCH --output=/dcai/users/%u/logs/neurallam.%j.log
+#SBATCH --error=/dcai/users/%u/logs/neurallam.%j.log
 
 echo "Started slurm job $SLURM_JOB_ID"
 
-<<<<<<< HEAD
 export CARTOPY_DATA_DIR=/dcai/projects/cu_0003/user_space/has/cartopy_features/
 export MLFLOW_TRACKING_URI="https://mlflow.dmi.dcs.dcai.dk" #sqlite:///mlflow.db #
 export MLFLOW_TRACKING_INSECURE_TLS=true
@@ -34,4 +33,21 @@ OMP_NUM_THREADS=56
 OMPI_MCA_coll_hcoll_enable=0
 set +a
 
-srun -ul python train_wrapper.py "$@"
+# remove the --venv_path argument from the args passed to this script and
+# assign it to the VENV_PATH variable
+args=("$@")
+for i in "${!args[@]}"; do
+    if [[ "${args[$i]}" == "--venv_path" ]]; then
+        VENV_PATH="${args[$i+1]}"
+        unset 'args[i]'
+        unset 'args[i+1]'
+    fi
+done
+
+echo "Using venv in ${VENV_PATH}"
+
+# source the virtual environment so that the python script can be run
+source ${VENV_PATH}/bin/activate
+
+# pass the rest of the args to the python script
+srun -ul python train_wrapper.py "${args[@]}"
