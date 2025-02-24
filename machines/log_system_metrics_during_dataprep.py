828c202e2464c653f5bfa372dafd7a2ecb831a15
Leif Denby <lcd@dmi.dk>
2025-02-24 10:56:02 +0100

    working tracking of dataprep

--- machines/log_system_metrics_during_dataprep.py
+++ machines/log_system_metrics_during_dataprep.py
@@ -5,15 +5,44 @@ import mllam_data_prep as mdp
 import signal
 import sys
 import urllib3
-import numpy as np
-import sklearn.linear_model  # Dummy model to trigger autologging
+import os
+from loguru import logger
 
 # supress warnings about insecure requests to MLflow tracking server
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 EXPERIMENT_NAME = "mllam-data-prep"
 
-def main(config_path):
+def _calc_directory_size(directory):
+    """Calculate the size of a directory in bytes."""
+    total = 0
+    with os.scandir(directory) as it:
+        for entry in it:
+            if entry.is_file():
+                try:
+                    total += os.path.getsize(entry.path)
+                except FileNotFoundError:
+                    # this can happen because dask writes temporary files that are deleted
+                    pass
+            elif entry.is_dir():
+                total += _calc_directory_size(entry.path)
+    return total
+
+def main(config_path, dataset_output_path):
+    """
+    Set up an MLflow experiment and log system metrics during data preparation.
+    This doesn't actually do any "machine learning", but using mlflow to track
+    system metrics is useful for debugging the dataset preperation.
+    
+    Parameters
+    ----------
+    config_path : str
+        Path to the configuration file used with mllam_data_prep to generate
+        the dataset. We log the configuration parameters to MLflow.
+    dataset_output_path : str
+        Path to the directory where the dataset is being generated. We log the
+        size of this directory to MLflow as the dataset is being generated.
+    """
     # Set experiment name
     mlflow.set_experiment(EXPERIMENT_NAME)
     
@@ -21,7 +50,11 @@ def main(config_path):
     config = mdp.Config.from_yaml_file(config_path)
 
     # Start MLflow Run
-    mlflow.start_run()
+    run = mlflow.start_run(log_system_metrics=True)
+    experiment_id = mlflow.get_experiment_by_name(EXPERIMENT_NAME).experiment_id
+    
+    logger.info(f"Logging system metrics to MLflow experiment '{EXPERIMENT_NAME}'")
+    logger.info(f"Run id: {run.info.run_id}, experiment id: {experiment_id}")
 
     mlflow.log_params(config.to_dict())
     
@@ -36,29 +69,16 @@ def main(config_path):
     # Register the signal handler for SIGINT (CTRL+C) and SIGTERM (kill command)
     signal.signal(signal.SIGINT, handle_exit)
     signal.signal(signal.SIGTERM, handle_exit)
-    
-    # Dummy model training to activate system monitoring
-    X_dummy = np.random.rand(100, 5)
-    y_dummy = np.random.rand(100)
-    model = sklearn.linear_model.LinearRegression()
-    model.fit(X_dummy, y_dummy)  # This triggers MLflow's system monitoring
 
     try:
         step = 0  # Initialize step counter
         while True:
-            # Collect system metrics
-            cpu_usage = psutil.cpu_percent(interval=1)
-            ram_usage = psutil.virtual_memory().percent
-            disk_usage = psutil.disk_usage("/").percent
-
-            # Log system metrics with step count
-            mlflow.log_metric("CPU_Usage", cpu_usage, step=step)
-            mlflow.log_metric("RAM_Usage", ram_usage, step=step)
-            mlflow.log_metric("Disk_Usage", disk_usage, step=step)
-
+            # compute the size of the dataset and log it
+            dataset_size = _calc_directory_size(dataset_output_path)
+            mlflow.log_metric("dataset_size_mb", dataset_size / 1024 / 1024, step=step)
             step += 1  # Manually increment step counter
 
-            time.sleep(2)  # Log every 10 seconds
+            time.sleep(10)  # Log every 10 seconds
 
     except KeyboardInterrupt:
         handle_exit(None, None)  # Ensures graceful shutdown if interrupted manually
@@ -68,6 +88,7 @@ if __name__ == "__main__":
     import argparse
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--config_path", type=str, required=True)
+    argparser.add_argument("--dataset_output_path", type=str, required=True)
     args = argparser.parse_args()
     
-    main(config_path=args.config_path)
+    main(config_path=args.config_path, dataset_output_path=args.dataset_output_path)
--- machines/slurm.mllam-data-prep.sh
+++ machines/slurm.mllam-data-prep.sh
@@ -21,7 +21,23 @@ source ${MLLAM_VENV_PATH}/bin/activate
 
 # the config path is the first argument for mllam_data_prep
 CONFIG_PATH=$1
+# assume third argument is the output path
+OUTPUT_PATH=$3
 
-python machines/log_system_metrics_during_dataprep.py --config_path=${CONFIG_PATH} & LOGGER_PID=$!
+# Use a python script (which internally uses mlflow) to run a background process that logs system metrics.
+# Explanation:
+# - `python log_system_metrics.py &`  
+#     -> Starts the Python script as a background process.
+#     -> The `&` ensures it runs in the background and doesn't block the terminal.
+# - `LOGGER_PID=$!`  
+#     -> Captures the Process ID (PID) of the last background process (`$!` refers to the last process run in the background).
+#     -> This allows us to reference the process later (e.g., to stop it with `kill $LOGGER_PID`).
+# - `disown`  
+#     -> Removes the background process from the shell’s job table.
+#     -> Prevents the process from being terminated if the shell session is closed.
+#     -> Output from the script remains visible in the terminal.
+
+python machines/log_system_metrics_during_dataprep.py --config_path=${CONFIG_PATH} --dataset_output_path=${OUTPUT_PATH} & LOGGER_PID=$! && disown
+
 srun -ul python -m mllam_data_prep "$@"
 kill $LOGGER_PID
